b62a09ea3136d237536a389bf16a06f61cd08f72
James Dumay <jdumay@cloudbees.com>
2018-12-15 16:30:38 +1100

    plot

--- src/components/Plots.js
+++ src/components/Plots.js
@@ -4,13 +4,44 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import {Line} from 'react-chartjs-2';
+
+const styles = {
+  card: {
+    width: '20%',
+    height: '20%'
+  }
+}
 
 class Plot extends React.Component {
   render() {
+    const { classes } = this.props;
+    let allPoints = this.props.plot.points.map((point) => point.value)
+    let data = {
+      datasets: [
+        {
+          fill: false,
+          data: allPoints,
+          type: 'line',
+          borderColor: '#EC932F',
+          backgroundColor: '#EC932F',
+          pointBorderColor: '#EC932F',
+          pointBackgroundColor: '#EC932F',
+          pointHoverBackgroundColor: '#EC932F',
+          pointHoverBorderColor: '#EC932F',
+        }
+      ]
+    }
+
+    let options = {
+      responsive: true,
+    }
+
     return (
-      <Card>
+      <Card className={classes}>
         <CardContent>
           <Typography variant="h5" component="h2">{this.props.plot.name}</Typography>
+          <Line data={data} options={options}/>
         </CardContent>
       </Card>
     )
@@ -19,11 +50,12 @@ class Plot extends React.Component {
 
 class Plots extends React.Component {
   render() {
+    const { classes } = this.props;
     let charts = []
     for (var i = 0; i < this.props.plots.length; i++) {
       charts.push((
         <Grid key={i} item>
-          <Plot plot={this.props.plots[i]}/>
+          <Plot classes={classes} plot={this.props.plots[i]}/>
         </Grid>
       ))
     }
@@ -35,4 +67,4 @@ class Plots extends React.Component {
   }
 }
 
-export default Plots;
+export default withStyles(styles)(Plots);
